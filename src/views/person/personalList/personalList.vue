18b7ccd2041fa36ca863f8c94fc608424c23e989
SeaHeart001 <m13871213462@163.com>
2020-08-20 17:01:14 +0800

    try use the lazy tree table

--- src/views/person/personalList/personalList.vue
+++ src/views/person/personalList/personalList.vue
@@ -1,13 +1,95 @@
 <template>
-  <div>personalList</div>
+  <div>
+    <div>异步树表格</div>
+    <el-table
+      :data="tableData"
+      row-key="id"
+      lazy
+      :load="load"
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+    >
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "personalList"
-}
+  name: "personalList",
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          children: [
+            {
+              id: Math.random(),
+              date: "2016-05-02",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1518 弄",
+            },
+          ],
+        },
+        {
+          id: 2,
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+          hasChildren: true,
+        },
+        {
+          id: 3,
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+          hasChildren: true,
+        },
+        {
+          id: 4,
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    load(tree, treeNode, resolve) {
+      console.log(tree, treeNode, resolve);
+      setTimeout(() => {
+        resolve([
+          {
+            id: Math.random(),
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄",
+            hasChildren: true,
+          },
+          {
+            id: Math.random(),
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄",
+            children: [
+              {
+                id: Math.random(),
+                date: "2016-05-01",
+                name: "王小虎",
+                address: "上海市普陀区金沙江路 1519 弄",
+              },
+            ],
+          },
+        ]);
+      }, 1000);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
